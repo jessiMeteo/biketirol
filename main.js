06e38a097a8623483455c25b5a93bcc332d0f245
jessimeteo <jessica.laengle@uibk.ac.at>
2025-05-22 16:34:39 +0200

    for of Schleife beginnen

--- main.js
+++ main.js
@@ -51,4 +51,11 @@ L.control.scale({
     imperial: false,
 }).addTo(map);
 
-
+//Etappennavigation über Pulldown-Menü
+console.log(ETAPPEN);
+for (let etappe of ETAPPEN){
+    console.log(etappe);
+    console.log(etappe.user);
+    console.log(etappe.nr);
+    console.log(etappe.titel);
+}
